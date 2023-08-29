f04a56b650082607b37827d642c8fdc80db67a85
skyriver1126 <sora.17.1126@gmail.com>
2023-08-29 19:13:59 +0900

    initial commit

--- intern/settings.py
+++ intern/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,12 @@ AUTH_USER_MODEL = "myapp.CustomUser"
 LOGIN_REDIRECT_URL = "friends"
 LOGIN_URL = "login"
 LOGOUT_REDIRECT_URL = "index"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+if os.path.isfile('.env'): # .envファイルが存在しない時にもエラーが発生しないようにする
+    env = environ.Env(DEBUG=(bool, False),)
+    environ.Env.read_env('.env')
+
+    DEBUG = env('DEBUG')
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
